47fd315eb3e8b507898132a7490aac4433d63008
sebastian de castelberg <sebu@kpricorn.org>
2010-10-15 14:30:04 +0200

    initial commit

---
+++ lib/Dancer/Plugin/RP.pm
@@ -0,0 +1,110 @@
+package Dancer::Plugin::RP;
+
+use warnings;
+use strict;
+
+=head1 NAME
+
+Dancer::Plugin::RP - The great new Dancer::Plugin::RP!
+
+=head1 VERSION
+
+Version 0.01
+
+=cut
+
+our $VERSION = '0.01';
+
+
+=head1 SYNOPSIS
+
+Quick summary of what the module does.
+
+Perhaps a little code snippet.
+
+    use Dancer::Plugin::RP;
+
+    my $foo = Dancer::Plugin::RP->new();
+    ...
+
+=head1 EXPORT
+
+A list of functions that can be exported.  You can delete this section
+if you don't export anything, such as for a purely object-oriented module.
+
+=head1 SUBROUTINES/METHODS
+
+=head2 function1
+
+=cut
+
+sub function1 {
+}
+
+=head2 function2
+
+=cut
+
+sub function2 {
+}
+
+=head1 AUTHOR
+
+Sebastian de Castelberg, C<< <sdecaste at cpan.org> >>
+
+=head1 BUGS
+
+Please report any bugs or feature requests to C<bug-dancer-plugin-rp at rt.cpan.org>, or through
+the web interface at L<http://rt.cpan.org/NoAuth/ReportBug.html?Queue=Dancer-Plugin-RP>.  I will be notified, and then you'll
+automatically be notified of progress on your bug as I make changes.
+
+
+
+
+=head1 SUPPORT
+
+You can find documentation for this module with the perldoc command.
+
+    perldoc Dancer::Plugin::RP
+
+
+You can also look for information at:
+
+=over 4
+
+=item * RT: CPAN's request tracker
+
+L<http://rt.cpan.org/NoAuth/Bugs.html?Dist=Dancer-Plugin-RP>
+
+=item * AnnoCPAN: Annotated CPAN documentation
+
+L<http://annocpan.org/dist/Dancer-Plugin-RP>
+
+=item * CPAN Ratings
+
+L<http://cpanratings.perl.org/d/Dancer-Plugin-RP>
+
+=item * Search CPAN
+
+L<http://search.cpan.org/dist/Dancer-Plugin-RP/>
+
+=back
+
+
+=head1 ACKNOWLEDGEMENTS
+
+
+=head1 LICENSE AND COPYRIGHT
+
+Copyright 2010 Sebastian de Castelberg.
+
+This program is free software; you can redistribute it and/or modify it
+under the terms of either: the GNU General Public License as published
+by the Free Software Foundation; or the Artistic License.
+
+See http://dev.perl.org/licenses/ for more information.
+
+
+=cut
+
+1; # End of Dancer::Plugin::RP
